2b570033e9e65339a26b3ac989935eea4bac0f34
happyaaron503 <blackknight1251@gmail.com>
2023-12-25 19:21:14 +0800

    average of comment

--- read.py
+++ read.py
@@ -3,7 +3,10 @@ count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip())
-        count += 1
-        if count % 1000 == 0:
-            print(count)
+
+sum = 0   
+for d in data:
+    sum = sum + len(d)
+
+print('每筆留言平均長度為', sum / len(data), '個字')
 
