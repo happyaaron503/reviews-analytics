63e4061e6c3b3ee6b35633b2141de288ca580a13
happyaaron503 <blackknight1251@gmail.com>
2023-12-31 16:56:12 +0800

    word count

--- read.py
+++ read.py
@@ -4,24 +4,59 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip())
 
-sum = 0   
-for d in data:
-    sum = sum + len(d)
+#print(data[0])
 
-print('每筆留言平均長度為', sum / len(data), '個字')
-
-new = []
+wc = {} # word_count
 for d in data:
-    if len(d) < 100:
-        new.append(d)
-print('共有', len(new), '筆留言長度小於100')
+    words = d.split()
+    for word in words:   
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+    #print(wc)
 
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
-#good = [d for d in data if 'good' in d] 
-print('共有', len(good), '筆留言包含good')
-print(good[0])
+for word in wc:
+    if wc[word] > 100000:
+        print(word, wc[word])
+print(len(wc))
+
+while True:
+    word = input('找啥: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過', wc[word], '次')
+    else:
+        print('沒出現過喔')
+print('結束')
+
+    
+
+
+
+
+
+
+# sum = 0   
+# for d in data:
+#     sum = sum + len(d)
+
+# print('每筆留言平均長度為', sum / len(data), '個字')
+
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+# print('共有', len(new), '筆留言長度小於100')
+
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+# #good = [d for d in data if 'good' in d] 
+# print('共有', len(good), '筆留言包含good')
+
+
 
 
